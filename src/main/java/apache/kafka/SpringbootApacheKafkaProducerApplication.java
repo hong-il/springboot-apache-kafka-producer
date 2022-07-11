cf4bcde2bd3c9e744dc52018aa215a66179f4ff7
hong-il <hongil.kim.dev@gmail.com>
2022-07-11 10:45:54 +0900

    Initial commit

---
+++ src/main/java/apache/kafka/SpringbootApacheKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package apache.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApacheKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/apache/kafka/SpringbootApacheKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package apache.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApacheKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
